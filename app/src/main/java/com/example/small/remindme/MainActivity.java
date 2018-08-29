e78766110e14750ed8cef4c86b61d7fd2008233e
lirguo <roostik21@gmail.com>
2018-08-29 16:49:16 +0300

    TabLayout & ViewPager

--- app/src/main/java/com/example/small/remindme/MainActivity.java
+++ app/src/main/java/com/example/small/remindme/MainActivity.java
@@ -1,18 +1,27 @@
 package com.example.small.remindme;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
-public class MainActivity extends Activity {
+import com.example.small.remindme.adapter.TabsPagerFragmentAdapter;
+
+public class MainActivity extends AppCompatActivity {
 
     private static final int LAYOUT = R.layout.activity_main;
 
     private Toolbar toolbar;
     private DrawerLayout drawerLayout;
+    private ViewPager viewPager;
+    private  TabLayout  tabLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,6 +30,9 @@ public class MainActivity extends Activity {
 
         initToolBar();
         initNavView();
+        initTabs();
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
     }
 
     private void initToolBar() {
@@ -37,6 +49,35 @@ public class MainActivity extends Activity {
 
     private void initNavView() {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                switch (menuItem.getItemId()){
+                    case R.id.actionNotificationItem:
+                        showNotificatiobTab();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void initTabs() {
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        TabsPagerFragmentAdapter adapter = new TabsPagerFragmentAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    private void showNotificatiobTab(){
+        viewPager.setCurrentItem(Constants.TAB_TWO);
     }
 
 }
