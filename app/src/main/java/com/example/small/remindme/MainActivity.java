dc5987eddbb9327f59435f62ca4f7d3651617aaa
lirguo <roostik21@gmail.com>
2018-08-29 15:19:09 +0300

    Added header to navigation and mock item in nav

--- app/src/main/java/com/example/small/remindme/MainActivity.java
+++ app/src/main/java/com/example/small/remindme/MainActivity.java
@@ -3,19 +3,24 @@ package com.example.small.remindme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
+    private static final int LAYOUT = R.layout.activity_main;
+
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolBar();
+        initNavView();
     }
 
     private void initToolBar() {
@@ -29,4 +34,9 @@ public class MainActivity extends Activity {
         });
         toolbar.inflateMenu(R.menu.menu);
     }
+
+    private void initNavView() {
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+    }
+
 }
